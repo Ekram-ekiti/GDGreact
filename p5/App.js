988ddc1086b6b470c9e9bb6b353ef2cee3b5d757
Ekram <ekru6482@gmail.com>
2025-03-18 21:39:46 +0300

    Create App.js

---
+++ p5/App.js
@@ -0,0 +1,11 @@
+import CharacterCounter from './components/CharacterCounter';
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <CharacterCounter />
+    </div>
+  );
+}
+
+export default App;
