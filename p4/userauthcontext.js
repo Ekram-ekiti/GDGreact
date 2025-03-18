980634157f76f7d147037ad45a5b0571ae6f590c
Ekram <ekru6482@gmail.com>
2025-03-18 21:33:26 +0300

    Create userauthcontext.js

---
+++ p4/userauthcontext.js
@@ -0,0 +1,18 @@
+import { createContext, useContext, useState } from "react";
+
+const UserAuthContext = createContext();
+
+export const UserAuthProvider = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = () => setIsLoggedIn(true);
+  const logout = () => setIsLoggedIn(false);
+
+  return (
+    <UserAuthContext.Provider value={{ isLoggedIn, login, logout }}>
+      {children}
+    </UserAuthContext.Provider>
+  );
+};
+
+export const useUserAuth = () => useContext(UserAuthContext);
